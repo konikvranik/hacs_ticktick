87cf24a50253c31f5c9dd9ab4158b004d2dbeaf9
pvranik <petr.vranik@mgm-tp.com>
2024-11-17 21:37:04 +0100

    import

--- custom_components/ticktick_todo/pyticktick/openapi_client/models/status.py
+++ custom_components/ticktick_todo/pyticktick/openapi_client/models/status.py
@@ -13,11 +13,9 @@
 
 import json
 import re  # noqa: F401
+import custom_components
 
 from aenum import Enum
-
-from . import Status
-
 
 class Status(int, Enum):
     """
@@ -31,6 +29,6 @@ class Status(int, Enum):
     NUMBER_2 = 2
 
     @classmethod
-    def from_json(cls, json_str: str) -> Status:
+    def from_json(cls, json_str: str) -> custom_components.ticktick_todo.pyticktick.openapi_client.models.status.Status:
         """Create an instance of Status from a JSON string"""
         return Status(json.loads(json_str))
