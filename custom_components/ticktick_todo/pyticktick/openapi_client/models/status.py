e1d290ce08c600bd0ead1638b8fa799cdc2043ef
pvranik <petr.vranik@mgm-tp.com>
2024-11-19 22:04:52 +0100

    test cleanup

--- custom_components/ticktick_todo/pyticktick/openapi_client/models/status.py
+++ custom_components/ticktick_todo/pyticktick/openapi_client/models/status.py
@@ -11,11 +11,13 @@
     Do not edit the class manually.
 """  # noqa: E501
 
+from __future__ import annotations
+
 import json
 import re  # noqa: F401
-import custom_components
 
 from aenum import Enum
+
 
 class Status(int, Enum):
     """
@@ -29,6 +31,6 @@ class Status(int, Enum):
     NUMBER_2 = 2
 
     @classmethod
-    def from_json(cls, json_str: str) -> 'Status':
+    def from_json(cls, json_str: str) -> Status:
         """Create an instance of Status from a JSON string"""
         return Status(json.loads(json_str))
