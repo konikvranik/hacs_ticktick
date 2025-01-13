50af789d38253e6b7c00448229a936b79c9ab7f7
Petr Vraník <Petr.Vranik@mgm-tp.com>
2025-01-13 17:47:33 +0100

    generate openapi

--- generate_client.sh
+++ generate_client.sh
@@ -1,5 +1,6 @@
 docker run --rm \
   -v ${PWD}:/local \
+  --user $(id -u):$(id -g) \
   openapitools/openapi-generator-cli generate \
   -i https://ticktick.com/openapi.yaml \
   -g python-pydantic-v1 \
